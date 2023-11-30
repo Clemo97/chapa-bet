8d9fb2fd31f016adc9cb0f575e5d333b20af0b57
Clemo97 <lumumbaclement@gmail.com>
2023-11-30 11:03:15 +0300

    database change

--- backend/session_auth/settings.py
+++ backend/session_auth/settings.py
@@ -82,15 +82,15 @@ WSGI_APPLICATION = 'session_auth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'session_auth',
-#         'USER': 'clement',
-#         'PASSWORD': 'clemolumz',
-#         'HOST': 'localhost'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'session_auth',
+        'USER': 'clement',
+        'PASSWORD': 'clemolumz',
+        'HOST': 'localhost'
+    }
+}
 
 # Define your DATABASES dictionary
 
